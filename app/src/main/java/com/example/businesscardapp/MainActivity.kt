0c0d5e310c6db080f3c3120450574b3f789d7913
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2023-11-15 16:20:57 +0530

    Added contact information card which displays phone number, social media link and email.

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -7,10 +7,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -79,14 +85,66 @@ fun IntroductionCard(
     }
 }
 
+@Composable
+fun ContactInformationCard(
+    mobileNumber: String,
+    socialMedia: String,
+    email: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = modifier
+                .padding(bottom = 10.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Call,
+                contentDescription = "Mobile_Number",
+                tint = Color(0, 109, 59)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = mobileNumber,
+                color = Color(41,54,44)
+            )
+        }
+        Row(modifier = modifier.padding(bottom = 10.dp)) {
+            Icon(
+                imageVector = Icons.Rounded.Share,
+                contentDescription = "Social_Media",
+                tint = Color(0, 109, 59)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = socialMedia,
+                color = Color(41,54,44)
+            )
+        }
+        Row {
+            Icon(
+                imageVector = Icons.Rounded.Email,
+                contentDescription = "Gmail",
+                tint = Color(0, 109, 59)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = email,
+                color = Color(41,54,44)
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardAppTheme {
-        IntroductionCard(
-            image = painterResource(id = R.drawable.android_logo),
-            name = stringResource(R.string.name_text),
-            designation = stringResource(R.string.designation_text)
+        ContactInformationCard(
+            mobileNumber = stringResource(R.string.mobile_number_text),
+            socialMedia = stringResource(R.string.social_media_text),
+            email = stringResource(R.string.email_text)
         )
     }
 }
