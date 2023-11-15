35c22ee60adee733e5fa6b8e77cd8ede3b5543c1
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2023-11-15 11:10:59 +0530

    Added introduction card which displays image, name and designation.

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.businesscardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,17 +44,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun IntroductionCard(
+    image: Painter,
+    name: String,
+    designation: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-    )
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier
+                .height(100.dp)
+                .width(100.dp)
+                .background(Color(7, 48, 66))
+        )
+        Text(
+            text = name,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.W400,
+            modifier = modifier.padding(10.dp),
+            color =  Color(42, 56, 45)
+        )
+        Text(
+            text = designation,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(26, 123, 76)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardAppTheme {
-        Greeting("Android")
+        IntroductionCard(
+            image = painterResource(id = R.drawable.android_logo),
+            name = stringResource(R.string.name_text),
+            designation = stringResource(R.string.designation_text)
+        )
     }
 }
